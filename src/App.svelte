e643cf30645e6c2d7a78be3bbbb7659a81075032
KianSabzi <sabzi.morteza@gmail.com>
2023-10-12 14:23:24 +0330

    Update App.svelte for loading user tasks

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,41 @@
 <script>
+   import TaskList from "./UserTasks/TaskList.svelte";
+
   
+let isFav = null;
+let tasks = [
+      {
+          id : "01",
+          title : "حل تمرینات گرامر پیشرفته - درس سوم",
+          dueDate : "1401/10/21",
+          progress : "75%",
+          isFav: !isFav
+
+      },
+      {
+          id : "02",
+          title : "تکمیل بخش احراز هویت پروژه وب شرکت فن پرداز داتیس",
+          dueDate : "1401/10/24",
+          progress : "45%",
+          isFav: !isFav
+      },
+      {
+          id : "03",
+          title : "مرور لغات 1100 - بخش 12",
+          dueDate : "1401/10/22",
+          progress : "90%",
+          isFav: !isFav
+      },
+      {
+          id : "04",
+          title : "گرامر پیشرفته ایلتس - صرف مجهول",
+          dueDate : "1401/12/10",
+          progress : "30%",
+          isFav: !isFav
+      }
+      
+  ]
+
 </script>
 
 <main>
@@ -9,3 +45,6 @@
 <style>
   
 </style>
+
+
+<TaskList {tasks}/>
