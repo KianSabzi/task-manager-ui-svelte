8bee5ab212a29a2a80c0eaa303347f1f267e27dd
KianSabzi <sabzi.morteza@gmail.com>
2023-10-12 18:55:32 +0330

    Update task list component and add task box

--- src/UserTasks/TaskList.svelte
+++ src/UserTasks/TaskList.svelte
@@ -1,7 +1,9 @@
 <script>
 // @ts-nocheck
-    import { Listgroup, ListgroupItem } from 'flowbite-svelte';
-    import { StarOutline} from "flowbite-svelte-icons"
+    import { Listgroup, ListgroupItem, NavHamburger } from 'flowbite-svelte';
+    import { LinkOutline, StarOutline} from "flowbite-svelte-icons"
+    import TaskDetails from './TaskDetails.svelte';
+    import TaskBox from './TaskBox.svelte';
     export let tasks;
 </script>
 
@@ -11,12 +13,12 @@
 
 <section id="tasklist">
     <h3 class="py-4 text-center text-xl font-medium text-gray-900 dark:text-white">کنکور ارشد</h3>
-    <Listgroup active items={tasks} let:item class="w-full md:w-auto space-y-3 p-2.5 relative">
-        <div class="flex flex-col p-2">
-            <div class=""><h2>{item.title}</h2></div>
-            <div><span class="text-orange-500 float-right">{item.dueDate}</span></div> 
-        </div>
-        <i class="absolute left-4 text-blue-700"><StarOutline class=""></StarOutline></i>        
-    </Listgroup>
+    {#each tasks as task}
+    <TaskBox 
+    title = {task.title}
+    dueDate = {task.dueDate}
+    progress = {task.progress}
+    />
+    {/each}
 
 </section>
