011aa20aa9bfedcf1c81d7b3c058f22a6c2015cf
KianSabzi <sabzi.morteza@gmail.com>
2023-10-13 19:20:56 +0330

    Update footer and fixed some ui issues

--- src/UI/Footer.svelte
+++ src/UI/Footer.svelte
@@ -3,3 +3,13 @@
 </script>
 
 
+<Footer>
+    <FooterCopyright href="/" by="KIANITA™" year={2023} />
+    <FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-600 dark:text-gray-400 sm:mt-0">
+      <FooterLink classLi="!ml-1" href="/">درباره ما</FooterLink>
+      <FooterLink classLi="!ml-4" href="/">حفظ حریم خصوصی</FooterLink>
+      <FooterLink classLi="!ml-3" href="/">تماس با ما</FooterLink>
+    </FooterLinkGroup>
+</Footer>
+
+
