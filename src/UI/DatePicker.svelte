65bbfcb1773f6e2c6d1fcd5ff3b186de3981af69
KianSabzi <sabzi.morteza@gmail.com>
2023-10-16 19:48:32 +0330

    Add date picker component and config it based on jalali lib

---
+++ src/UI/DatePicker.svelte
@@ -0,0 +1,10 @@
+<script>
+// @ts-ignore
+jalaliDatepicker.startWatch({
+   time: "true"
+})
+</script>  
+
+
+<input class="w-48 text-center" data-jdp name="date_picker" placeholder="انتخاب تاریخ" />
+  
