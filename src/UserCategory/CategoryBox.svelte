d2c2c1e9b2dd8787e97a3318e0043316ded2d9bc
KianSabzi <sabzi.morteza@gmail.com>
2023-10-17 16:59:08 +0330

    Initial new category and design implement

---
+++ src/UserCategory/CategoryBox.svelte
@@ -0,0 +1,10 @@
+<script>
+        import {Input, Select, Label, Button} from 'flowbite-svelte';
+</script>
+
+
+
+<div class="container space-y-6 pt-4">
+    <Input class="w-full text-black-700" placeholder="نام دسته"/>
+    <Button color="light" type="submit">ثبت</Button>
+</div>
